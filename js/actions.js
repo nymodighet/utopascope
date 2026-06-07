12e3cd35f84ae3ecf2d234ade8de64c583261a59
ardenperth <ardenperth@proton.me>
2026-06-07 12:07:13 -0400

    Undo like-column style and redo Applause JS

--- js/actions.js
+++ js/actions.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
     $('a.right-link').click(function(event){
         event.preventDefault();
         linkLocation = this.href;
+	    $('.like-column').each(function() {
+	        $(this).css('top', $(this).position().top + 'px');
+	    });
         $($body).fadeOut(800, redirectPage); 
         $('.fade-out').addClass('fading-out-right');    
     });
@@ -29,6 +32,9 @@ $(document).ready(function() {
     $('a.left-link').click(function(event){
         event.preventDefault();
         linkLocation = this.href;
+	    $('.like-column').each(function() {
+	        $(this).css('top', $(this).position().top + 'px');
+	    });
         $($body).fadeOut(800, redirectPage); 
         $('.fade-out').addClass('fading-out-left');    
     });
